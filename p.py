8b92a1ecbc3690e405ac0b0b06be7358f91bd637
Tibor Simon <tibor@tiborsimon.io>
2016-04-25 19:26:46 +0200

    Python based implementation started.

---
+++ p.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+
+def main(args):
+    print(args)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ test/test_main.py
@@ -0,0 +1,7 @@
+import unittest
+from p import main
+
+
+class BasicFunctionalityTests(unittest.TestCase):
+    def test__basic_testcase(self):
+        pass
