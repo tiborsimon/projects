14900cdd1de816dcf0cc19b201764cfb03f4b846
Tibor Simon <tibor@tiborsimon.io>
2016-05-01 17:28:37 +0200

    Config module finished.

--- projects/config.py
+++ projects/config.py
@@ -17,51 +17,104 @@ There are optional parameters as well:
                       your custom projects in a list, and put the project files into the
                       ~/.p/plugins directory.
 
+API:
+    get()           Returns the validated configuration as a dictionary. In case of error throws
+                    a ConfigError with a displayable error message.
+
+Raises:
+    ConfigError     in case of config related problems:
+                        - invalid project file json syntax
+                        - missing mandatory project file key
+                        - invalid project file value
+                        - invalid project file key
+                        - project file cannot be written if missing
 """
 
 import os
 import json
 
-default_config = {
+
+class ConfigError(Exception):
+    pass
+
+
+def get():
+    """ Only API function for the config module.
+
+
+    :return: {dict}     loaded validated configuration.
+    """
+    config = {}
+    try:
+        config = _load_config()
+    except IOError:
+        try:
+            _create_default_config()
+            config = _load_config()
+        except IOError as e:
+            raise ConfigError('Config file ({}) cannot be created. '
+                              'IOError: {}'.format(_config_file, e.message))
+    except SyntaxError as e:
+        raise ConfigError('Invalid JSON format in config file ({}). '
+                          'SyntaxError: {}'.format(_config_file, e.message))
+
+    try:
+        _validate(config)
+    except KeyError as e:
+        raise ConfigError('Missing mandatory key "{}" '
+                          'in config file ({}).'.format(e.message, _config_file))
+    except SyntaxError as e:
+        raise ConfigError('Invalid key "{}" '
+                          'in config file ({}).'.format(e.message, _config_file))
+    except ValueError as e:
+        raise ConfigError('Invalid value for key "{}" '
+                          'in config file ({}).'.format(e.message, _config_file))
+    return config
+
+
+_config_file = '~/.prc'
+
+_default_config = {
     'projects-path': '~/projects'
 }
 
-optional_config = {
+_optional_config = {
     'number-color': 'yellow',
     'highlight-color': 'yellow',
     'plugins': []
 }
 
-def get_config_path():
-    return os.path.expanduser('~/.prc')
+
+def _get_config_path():
+    return os.path.expanduser(_config_file)
 
 
-def load_config():
+def _load_config():
     """ Config loading
     Raises:
-        IOError     - on missing config file
-        SyntaxError - on invalid json syntax
+        IOError         on missing config file
+        SyntaxError     on invalid json syntax
     :return: {dict} loaded but unvalidated config
     """
-    config_path = get_config_path()
+    config_path = _get_config_path()
     with open(config_path, 'r') as f:
         return json.load(f)
 
 
-def validate(config):
+def _validate(config):
     """ Config validation
     Raises:
-        KeyError    - on missing mandatory key
-        SyntaxError - on invalid key
-        ValueError  - on invalid value for key
+        KeyError        on missing mandatory key
+        SyntaxError     on invalid key
+        ValueError      on invalid value for key
     :param config: {dict} config to validate
     :return: None
     """
-    for mandatory_key in default_config.keys():
+    for mandatory_key in _default_config.keys():
         if mandatory_key not in config:
             raise KeyError(mandatory_key)
-    full_config = default_config.copy()
-    full_config.update(optional_config)
+    full_config = _default_config.copy()
+    full_config.update(_optional_config)
     for key in config.keys():
         if key not in full_config:
             raise SyntaxError(key)
@@ -69,29 +122,14 @@ def validate(config):
             raise ValueError(key)
 
 
-def create_default_config():
+def _create_default_config():
     """ Writes the full default configuration to the appropriate place.
+    Raises:
+        IOError  - on unsuccesful file write
     :return: None
     """
-    config_path = get_config_path()
-    full_config = default_config.copy()
-    full_config.update(optional_config)
+    config_path = _get_config_path()
+    full_config = _default_config.copy()
+    full_config.update(_optional_config)
     with open(config_path, 'w+') as f:
         json.dump(f, full_config)
-
-
-def get():
-    config = {}
-    try:
-        config = load_config()
-        validate(config)
-    except Exception as e:
-        # TODO: add call to error display
-        try:
-            create_default_config()
-            current_config = load_config()
-        except IOError:
-            # TODO: cannot create default config
-            pass
-    return config
-
--- test/test_config.py
+++ test/test_config.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from unittest import TestCase
+
 try:
     import mock
 except ImportError:
@@ -20,7 +21,7 @@ class Path(TestCase):
 
     @mock.patch.object(config, 'os', autospec=True)
     def test__path_expanded_correctly(self, mock_os):
-        config.get_config_path()
+        config._get_config_path()
         mock_os.path.expanduser.assert_called_with('~/.prc')
 
 
@@ -30,53 +31,54 @@ class Loading(TestCase):
     @mock.patch.object(config, 'os', autospec=True)
     def test__config_path_required_correctly(self, mock_os, mock_json):
         with mock.patch(open_mock_string):
-            config.load_config()
+            config._load_config()
         mock_os.path.expanduser.assert_called_with('~/.prc')
 
     @mock.patch.object(config, 'json', autospec=True)
-    @mock.patch.object(config, 'get_config_path', autospec=True)
+    @mock.patch.object(config, '_get_config_path', autospec=True)
     def test__config_file_is_opened_from_the_right_path(self, mock_path, mock_json):
         dummy_config_path = '/config/path'
         mock_path.return_value = dummy_config_path
         with mock.patch(open_mock_string) as mock_open:
-            config.load_config()
+            config._load_config()
             mock_open.assert_called_with(dummy_config_path, 'r')
 
     @mock.patch.object(config, 'json', autospec=True)
     def test__file_not_found__raises_error(self, mock_json):
         mock_open = mock.MagicMock(side_effect = IOError())
-        with self.assertRaises(IOError):
+        with self.assertRaises(Exception) as cm:
             with mock.patch(open_mock_string, mock_open):
-                config.load_config()
+                config._load_config()
+        self.assertEqual(cm.exception.__class__, IOError)
 
     @mock.patch.object(config, 'json', autospec=True)
     def test__parsed_config_file_is_returned(self, mock_json):
-        dummy_config = {
-            'dummy': 'config'
-        }
+        dummy_config = {'dummy': 'config'}
         mock_json.load.return_value = dummy_config
         with mock.patch(open_mock_string):
-            result = config.load_config()
+            result = config._load_config()
         self.assertEqual(dummy_config, result)
 
     @mock.patch.object(config, 'json', autospec=True)
     def test__invalid_json_syntax__raises_error(self, mock_json):
         mock_json.load.side_effect = SyntaxError('json invalid')
-        with self.assertRaises(SyntaxError):
+        with self.assertRaises(Exception) as cm:
             with mock.patch(open_mock_string):
-                config.load_config()
+                config._load_config()
+        self.assertEqual(cm.exception.__class__, SyntaxError)
 
 
 class Validation(TestCase):
 
     def test__mandatory_keys_present__no_exception_raised(self):
-        dc = config.default_config
-        config.validate(dc)
+        dc = config._default_config
+        config._validate(dc)
 
     def test__missing_mandatory_key__raises_key_error(self):
-        with self.assertRaises(KeyError):
+        with self.assertRaises(Exception) as cm:
             dummy_config = {}
-            config.validate(dummy_config)
+            config._validate(dummy_config)
+        self.assertEqual(cm.exception.__class__, KeyError)
 
     def test__invalid_key__raises_syntax_error(self):
         invalid_config = {
@@ -84,59 +86,119 @@ class Validation(TestCase):
             'selection-mode-index-not-fuzzy': True,
             'invalid-key': 42
         }
-        with self.assertRaises(SyntaxError):
-            config.validate(invalid_config)
+        with self.assertRaises(Exception) as cm:
+            config._validate(invalid_config)
+        self.assertEqual(cm.exception.__class__, SyntaxError)
 
     def test__invalid_value__raises_value_error(self):
         invalid_config = {
             'projects-path': 42
         }
-        with self.assertRaises(ValueError):
-            config.validate(invalid_config)
+        with self.assertRaises(Exception) as cm:
+            config._validate(invalid_config)
+        self.assertEqual(cm.exception.__class__, ValueError)
 
 
 class Creation(TestCase):
 
-    @mock.patch.object(config, 'get_config_path', autospec=True)
+    @mock.patch.object(config, '_get_config_path', autospec=True)
     @mock.patch.object(config, 'json', autospec=True)
     def test__config_is_written_to_the_right_place(self, mock_json, mock_path):
         dummy_path = '/config/path'
         mock_path.return_value = dummy_path
         with mock.patch(open_mock_string) as mock_open:
-            config.create_default_config()
+            config._create_default_config()
             mock_open.assert_called_with(dummy_path, 'w+')
+
+    @mock.patch.object(config, '_get_config_path', autospec=True)
+    @mock.patch.object(config, 'json', autospec=True)
+    def test__file_cannot_be_written__raises_error(self, mock_json, mock_path):
+        with mock.patch(open_mock_string) as mock_open:
+            mock_open.side_effect = IOError()
+            with self.assertRaises(Exception) as cm:
+                config._create_default_config()
+            self.assertEqual(cm.exception.__class__, IOError)
 
     @mock.patch.object(config, 'json', autospec=True)
     def test__json_file_is_written_with_the_full_configuration(self, mock_json):
-        full_config = config.default_config.copy()
-        full_config.update(config.optional_config)
+        full_config = config._default_config.copy()
+        full_config.update(config._optional_config)
         mock_open = mock.mock_open()
         with mock.patch(open_mock_string, mock_open):
-            config.create_default_config()
+            config._create_default_config()
         mock_json.dump.assert_called_with(mock_open.return_value, full_config)
 
 
 class Getter(TestCase):
 
-    @mock.patch.object(config, 'load_config', autospec=True)
-    def test__loaded_config_saved_as_a_global_variable(self, mock_load):
-        dummy_config = config.default_config
+    @mock.patch.object(config, '_load_config', autospec=True)
+    def test__loaded_config_returned(self, mock_load):
+        dummy_config = config._default_config
         mock_load.return_value = dummy_config
         result = config.get()
         self.assertEqual(dummy_config, result)
 
-    @mock.patch.object(config, 'load_config', autospec=True)
-    @mock.patch.object(config, 'create_default_config', autospec=True)
+    @mock.patch.object(config, '_load_config', autospec=True)
+    @mock.patch.object(config, '_create_default_config', autospec=True)
     def test__config_file_not_exits__creates_new_one(self, mock_create, mock_load):
-        mock_load.return_value = config.default_config
-        mock_load.side_effect = IOError()
+        mock_load.side_effect = [IOError(), config._default_config]
         config.get()
         mock_create.assert_called_with()
 
-    @mock.patch.object(config, 'load_config', autospec=True)
-    @mock.patch.object(config, 'validate', autospec=True)
+    @mock.patch.object(config, '_load_config', autospec=True)
+    @mock.patch.object(config, '_create_default_config', autospec=True)
+    def test__config_file_cannot_be_written__raises_config_error(self, mock_create, mock_load):
+        mock_load.side_effect = IOError()
+        mock_create.side_effect = IOError('Some error..')
+        with self.assertRaises(Exception) as cm:
+            config.get()
+        self.assertEqual(cm.exception.__class__, config.ConfigError)
+        self.assertTrue('Config file ({}) cannot be created. '
+                        'IOError: Some error..'.format(config._config_file) in cm.exception)
+
+    @mock.patch.object(config, '_load_config', autospec=True)
+    def test__invalid_json_syntax__raises_config_error(self, mock_load):
+        mock_load.side_effect = SyntaxError('Some syntax error..')
+        with self.assertRaises(Exception) as cm:
+            config.get()
+        self.assertEqual(cm.exception.__class__, config.ConfigError)
+        self.assertTrue('Invalid JSON format in config file ({}). '
+                        'SyntaxError: Some syntax error..'.format(config._config_file) in cm.exception)
+
+    @mock.patch.object(config, '_load_config', autospec=True)
+    @mock.patch.object(config, '_validate', autospec=True)
     def test__loaded_config_gets_validated(self, mock_validate, mock_load):
-        dummy_config = config.default_config
+        dummy_config = config._default_config
         mock_load.return_value = dummy_config
         config.get()
         mock_validate.assert_called_with(dummy_config)
+
+    @mock.patch.object(config, '_load_config', autospec=True)
+    @mock.patch.object(config, '_validate', autospec=True)
+    def test__missing_mandatory_key__raises_config_error(self, mock_validate, mock_load):
+        mock_validate.side_effect = KeyError('projects-path')
+        with self.assertRaises(Exception) as cm:
+            config.get()
+        self.assertEqual(cm.exception.__class__, config.ConfigError)
+        self.assertTrue('Missing mandatory key "projects-path" '
+                        'in config file ({}).'.format(config._config_file) in cm.exception)
+
+    @mock.patch.object(config, '_load_config', autospec=True)
+    @mock.patch.object(config, '_validate', autospec=True)
+    def test__invalid_key__raises_config_error(self, mock_validate, mock_load):
+        mock_validate.side_effect = SyntaxError('invalid-key')
+        with self.assertRaises(Exception) as cm:
+            config.get()
+        self.assertEqual(cm.exception.__class__, config.ConfigError)
+        self.assertTrue('Invalid key "invalid-key" '
+                        'in config file ({}).'.format(config._config_file) in cm.exception)
+
+    @mock.patch.object(config, '_load_config', autospec=True)
+    @mock.patch.object(config, '_validate', autospec=True)
+    def test__invalid_value_for_key__raises_config_error(self, mock_validate, mock_load):
+        mock_validate.side_effect = ValueError('key')
+        with self.assertRaises(Exception) as cm:
+            config.get()
+        self.assertEqual(cm.exception.__class__, config.ConfigError)
+        self.assertTrue('Invalid value for key "key" '
+                        'in config file ({}).'.format(config._config_file) in cm.exception)
