fadd3277747e016ff77954f9f2cc7e3f5c6ac784
Tibor Simon <tibor@tiborsimon.io>
2016-06-07 14:35:17 +0200

    Progress

--- projects/filter.py
+++ projects/filter.py
@@ -9,12 +9,16 @@ def _get_pattern(keys):
     return ret
 
 
+def weight_item(item):
+    item['weight'] = 0
+
+
 class Filter(object):
     def __init__(self, data):
         self.lines = data
         self.keys = ''
 
-    def add_key(self, key):
+    def generate_data_stucture(self, key):
         self.keys += key
         pattern = re.compile(_get_pattern(self.keys))
 
@@ -22,14 +26,13 @@ class Filter(object):
 
         for line in self.lines:
             m = pattern.search(line)
+            selection = m.regs[1:] if m else ()
             ret.append({
-                'item': line,
-                'selection': [
-                    m.regs[1]
-                ]
+                'line': line,
+                'selection': selection
             })
         return ret
 
-
-
-
+    def add_key(self, key):
+        structure = self.generate_data_stucture(key)
+        return structure
--- test/test_filter.py
+++ test/test_filter.py
@@ -17,7 +17,7 @@ except ImportError:
 from projects import filter
 
 
-class FilterTests(TestCase):
+class FilterTestsAPI(TestCase):
     def test__filter_can_be_initialized(self):
         input = [
             'item 1',
@@ -25,6 +25,57 @@ class FilterTests(TestCase):
         ]
         f = filter.Filter(input)
 
+    def test__no_match_returns_empty_selection(self):
+        init = [
+            'abc'
+        ]
+        f = filter.Filter(init)
+        key = 'd'
+        expected = [
+            {
+                'line': init[0],
+                'selection': ()
+            }
+        ]
+        result = f.add_key(key)
+        self.assertEqual(expected, result)
+
+    def test__result_for_one_line_with_one_match(self):
+        init = [
+            'abcd'
+        ]
+        f = filter.Filter(init)
+        key = 'c'
+        expected = [
+            {
+                'line': init[0],
+                'selection': (
+                    (2,3),
+                )
+            }
+        ]
+        result = f.add_key(key)
+        self.assertEqual(expected, result)
+
+    def test__result_for_one_line_with_two_matches(self):
+        init = [
+            'abcdc'
+        ]
+        f = filter.Filter(init)
+        key = 'cd'
+        expected = [
+            {
+                'line': init[0],
+                'selection': (
+                    (2,3),
+                    (3,4)
+                )
+            }
+        ]
+        result = f.add_key(key)
+        self.assertEqual(expected, result)
+
+class SearchPatternGeneration(TestCase):
     def test__pattern_generation_1(self):
         keys = 'c'
         expected = '[^c]*(c)'
@@ -37,19 +88,48 @@ class FilterTests(TestCase):
         result = filter._get_pattern(keys)
         self.assertEqual(expected, result)
 
-    def test__filter_reacts_to_the_first_letter(self):
-        init = [
-            'abcd'
-        ]
-        f = filter.Filter(init)
-        key = 'c'
-        expected = [
-            {
-                'item': 'abcd',
-                'selection': [
-                    (2,3)
-                ]
-            }
-        ]
-        result = f.add_key(key)
-        self.assertEqual(expected, result)
+class SortAlgorithm(TestCase):
+    def test__one_long_line__first_character_match__returns_zero(self):
+        item = {
+            'line': 'a',
+            'selection': (
+                (0,1),
+            )
+        }
+        expected = 0
+        filter.weight_item(item)
+        self.assertEqual(expected, item['weight'])
+
+    def test__two_long_line__all_character_match__returns_zero(self):
+        item = {
+            'line': 'aa',
+            'selection': (
+                (0,2),
+            )
+        }
+        expected = 0
+        filter.weight_item(item)
+        self.assertEqual(expected, item['weight'])
+
+    def test__three_long_line__all_character_match__returns_zero(self):
+        item = {
+            'line': 'aaa',
+            'selection': (
+                (0,3),
+            )
+        }
+        expected = 0
+        filter.weight_item(item)
+        self.assertEqual(expected, item['weight'])
+
+    def test__three_long_line__first_two_characters_match__returns_zero(self):
+        item = {
+            'line': 'aaa',
+            'selection': (
+                (0,2),
+            )
+        }
+        expected = 1
+        filter.weight_item(item)
+        self.assertEqual(expected, item['weight'])
+
