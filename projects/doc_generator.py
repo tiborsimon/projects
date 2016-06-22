85ce27d6699c1d96995ec34b3e2020e1730b2e08
Tibor Simon <tibor@tiborsimon.io>
2016-06-23 00:35:31 +0200

    Projectfile extended.

--- projects/doc_generator.py
+++ projects/doc_generator.py
@@ -4,4 +4,4 @@ from pyfiglet import Figlet
 def generate_doc(data):
     f = Figlet(font='big')
     title = f.renderText(data['name'])
-    return title + '\nThe actual doc..'
+    return title + repr(data)
